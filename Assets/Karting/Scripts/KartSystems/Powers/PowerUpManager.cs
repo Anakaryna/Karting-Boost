6cdaa261490cacaa27a311bf763da1c23a667bf5
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-06-23 21:36:14 +0200

    Add mushroom animation

--- Assets/Karting/Scripts/KartSystems/Powers/PowerUpManager.cs
+++ Assets/Karting/Scripts/KartSystems/Powers/PowerUpManager.cs
@@ -1,26 +1,44 @@
 using UnityEngine;
+using System.Collections.Generic;
 using KartGame.KartSystems;
 
 public class PowerUpManager : MonoBehaviour
 {
-    public GameObject StoredPowerUp { get; private set; }
-    public bool HasPowerUp { get; private set; } = false;
+    private GameObject storedPowerUp;
+    private bool hasPowerUp = false;
     private ArcadeKart arcadeKart;
+    
+    private Dictionary<string, GameObject> powerUpModels = new Dictionary<string, GameObject>();
 
     void Awake()
     {
         arcadeKart = GetComponent<ArcadeKart>();
+
+        // Find and deactivate all power-up models
+        GameObject powerUpModelsParent = GameObject.Find("PowerUpModels");
+        foreach (Transform child in powerUpModelsParent.transform)
+        {
+            powerUpModels[child.name] = child.gameObject;
+            child.gameObject.SetActive(false);
+        }
     }
 
     public void StorePowerUp(GameObject powerUp)
     {
-        StoredPowerUp = powerUp;
-        HasPowerUp = true;
+        storedPowerUp = powerUp;
+        hasPowerUp = true;
+
+        // Get the power-up ID and activate the corresponding model
+        string powerUpID = powerUp.name; // Assumes the power-up's GameObject name matches the model name
+        if (powerUpModels.ContainsKey(powerUpID))
+        {
+            powerUpModels[powerUpID].SetActive(true);
+        }
     }
 
     private void Update()
     {
-        if (HasPowerUp && arcadeKart.Input.UsePowerUp)
+        if (hasPowerUp && arcadeKart.Input.UsePowerUp)
         {
             ActivateStoredPowerUp();
         }
@@ -28,9 +46,11 @@ public class PowerUpManager : MonoBehaviour
 
     private void ActivateStoredPowerUp()
     {
-        if (StoredPowerUp != null)
+        if (storedPowerUp != null)
         {
-            var mushroomPowerUp = StoredPowerUp.GetComponent<MushroomPowerUp>();
+            string powerUpID = storedPowerUp.name;
+
+            var mushroomPowerUp = storedPowerUp.GetComponent<MushroomPowerUp>();
             if (mushroomPowerUp != null)
             {
                 arcadeKart.AddPowerup(new ArcadeKart.StatPowerup
@@ -43,8 +63,14 @@ public class PowerUpManager : MonoBehaviour
                     MaxTime = mushroomPowerUp.duration
                 });
 
-                HasPowerUp = false;
-                StoredPowerUp = null;
+                hasPowerUp = false;
+                storedPowerUp = null;
+
+                // Deactivate the power-up model
+                if (powerUpModels.ContainsKey(powerUpID))
+                {
+                    powerUpModels[powerUpID].SetActive(false);
+                }
             }
         }
     }
